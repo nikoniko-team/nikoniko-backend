7288a4c82dbcd79d0519f5ba9ae205cfd6967570
Lucas Moraes <lucas.moraes@avenuecode.com>
2019-10-02 19:51:40 -0300

    Removendo segurança

--- src/main/java/br/com/senac/nikoniko/config/SwaggerConfiguration.java
+++ src/main/java/br/com/senac/nikoniko/config/SwaggerConfiguration.java
@@ -1,34 +1,20 @@
 package br.com.senac.nikoniko.config;
 
-import br.com.senac.nikoniko.security.utils.JwtTokenUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.ApiKeyVehicle;
-import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @Profile("dev")
 @EnableSwagger2
 public class SwaggerConfiguration {
-
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
-
-    @Autowired
-    @Qualifier("JwtUserDetails")
-    private UserDetailsService userDetailsService;
 
     @Bean
     public Docket api() {
@@ -44,18 +30,4 @@ public class SwaggerConfiguration {
             .build();
     }
 
-    @Bean
-    public SecurityConfiguration security() {
-        String token;
-        try {
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername("admin@nikoniko.com.br");
-            token = this.jwtTokenUtil.obterToken(userDetails);
-        } catch (Exception e) {
-            token = "";
-        }
-
-        //noinspection deprecation
-        return new SecurityConfiguration(null, null, null, null, "Bearer " + token, ApiKeyVehicle.HEADER,
-            "Authorization", ",");
-    }
 }
