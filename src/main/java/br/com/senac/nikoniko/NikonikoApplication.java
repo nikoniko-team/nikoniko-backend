300c1e6bd76b4ab43aa84a91abd6abb26ebb0e1b
Leandro Kersting de Freitas <leandro@frekele.org>
2019-10-07 01:26:48 -0300

    showAppVersionInfo() on startup app.

--- src/main/java/br/com/senac/nikoniko/NikonikoApplication.java
+++ src/main/java/br/com/senac/nikoniko/NikonikoApplication.java
@@ -1,5 +1,7 @@
 package br.com.senac.nikoniko;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NikonikoApplication {
 
     public static void main(String[] args) {
+        showAppVersionInfo();
         SpringApplication.run(NikonikoApplication.class, args);
+    }
+
+
+    private static void showAppVersionInfo() {
+        Logger logger = LoggerFactory.getLogger(NikonikoApplication.class);
+        logger.info("");
+        logger.info("########################################################################################");
+        logger.info("## |  java.version  |   " + System.getProperty("java.version"));
+        logger.info("## |  java.home     |   " + System.getProperty("java.home"));
+        logger.info("## |  java.vendor   |   " + System.getProperty("java.vendor"));
+        logger.info("## |  os.arch       |   " + System.getProperty("os.arch"));
+        logger.info("## |  os.name       |   " + System.getProperty("os.name"));
+        logger.info("## |  os.version    |   " + System.getProperty("os.version"));
+        logger.info("## |  spring-boot   |   " + SpringBootApplication.class.getPackage().getImplementationVersion());
+        logger.info("########################################################################################");
+        logger.info("");
     }
 
 }
