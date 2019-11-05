590ef310b6d4a5352e4c2c90906fc0afbfb04af0
Lucas Moraes <lucas.moraes@avenuecode.com>
2019-11-05 20:00:01 -0300

    Adding ModelMapper to project

--- src/main/java/br/com/senac/nikoniko/services/RecordServiceImpl.java
+++ src/main/java/br/com/senac/nikoniko/services/RecordServiceImpl.java
@@ -1,14 +1,27 @@
 package br.com.senac.nikoniko.services;
 
 import br.com.senac.nikoniko.dtos.in.InRecordDto;
+import br.com.senac.nikoniko.entities.Record;
+import br.com.senac.nikoniko.repositories.RecordRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RecordServiceImpl implements RecordService {
 
+    private final ModelMapper modelMapper;
+    private final RecordRepository recordRepository;
+
+    public RecordServiceImpl(ModelMapper modelMapper, RecordRepository recordRepository) {
+        this.modelMapper = modelMapper;
+        this.recordRepository = recordRepository;
+    }
+
     @Override
     public void save(InRecordDto inRecordDto, Long teamId, Long userId) {
-        
+        var record = modelMapper.map(inRecordDto, Record.class);
+
+        recordRepository.save(record);
     }
 
 }
