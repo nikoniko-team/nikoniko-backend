6a974b4a5406e442d64d306123598d07d611b2de
Leandro Kersting de Freitas <leandro@frekele.org>
2019-12-11 15:36:37 -0300

    .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")

--- src/main/java/br/com/senac/nikoniko/config/CorsConfiguration.java
+++ src/main/java/br/com/senac/nikoniko/config/CorsConfiguration.java
@@ -10,13 +10,11 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
         registry.addMapping("/**")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
             .allowedOrigins("*")
             .allowedHeaders("*")
-            .allowCredentials(true)
-        ;
+            .allowCredentials(true);
     }
 
 }
